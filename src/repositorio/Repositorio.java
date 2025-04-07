72584c973d4150dba39946e8d026d1248ef1599b
Ricardo06-12 <ricardotmilian@gmail.com>
2025-04-07 18:43:48 -0500

    actualizado

--- src/repositorio/Repositorio.java
+++ src/repositorio/Repositorio.java
@@ -6,7 +6,7 @@ public class Repositorio {
 
    
     public static void main(String[] args) {
-         System.out.println("buenas tardes");
+         System.out.println("buen dia ");
     }
     
 }
